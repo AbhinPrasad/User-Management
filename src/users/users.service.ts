9c1d19fb1daf636c8826cc275c5575a4fe59ce4f
Abhin Prasad <107907575+Abhin34@users.noreply.github.com>
2025-02-18 20:42:55 +0530

    imported users module in auth module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,24 +8,28 @@ import { msg } from 'src/common/constants/message.constants';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+  constructor(@InjectRepository(User) private users: Repository<User>) {}
 
   async createUser(
     userDto: CreateUserDto,
     createdBy: ReqInfo,
   ): Promise<ApiResponse> {
-    const userExists = await this.userRepo.findOne({
+    const userExists = await this.users.findOne({
       where: { email: userDto.email },
     });
     if (userExists) {
       throw new ConflictException(msg.emailExists);
     }
 
-    const user = this.userRepo.create({ createdBy, ...userDto });
-    const newUser = await this.userRepo.save(user);
+    const user = this.users.create({ createdBy, ...userDto });
+    const newUser = await this.users.save(user);
 
     return newUser.id
       ? { message: msg.createUserSuccess, userId: newUser.id }
       : { message: msg.createUserFailed };
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.users.findOne({ where: { email } });
   }
 }
