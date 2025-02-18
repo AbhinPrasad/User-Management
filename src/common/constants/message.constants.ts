7c0bd599868e0e18b8dc282c4917f5c1b5dd5656
Abhin Prasad <107907575+Abhin34@users.noreply.github.com>
2025-02-18 19:28:13 +0530

    message constants

---
+++ src/common/constants/message.constants.ts
@@ -0,0 +1,13 @@
+export const validationMsg = {
+  invalidPassword:
+    'Password must be at least 8 characters long and contain at least one uppercase, one number, and one special character.',
+};
+
+export const msg = {
+  somethingWentWrong: 'Something Went Wrong!',
+  emailExists: 'User with same email already exists.',
+  createUserSuccess: 'User created successfully.',
+  createUserFailed: 'Unable to create user at the moment.',
+  getUsersListSuccess: 'Users list retrieved successfully.',
+  noRecordsFound: 'No records found.',
+};
