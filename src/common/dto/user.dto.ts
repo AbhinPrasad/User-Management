48a8452de762167211e496b3f44925fce9bf50c9
Abhin Prasad <107907575+Abhin34@users.noreply.github.com>
2025-02-18 19:35:18 +0530

    validation pipe init

--- src/common/dto/user.dto.ts
+++ src/common/dto/user.dto.ts
@@ -1,8 +1,27 @@
 import { UserRole } from '../constants/db.constants';
+import {
+  IsEmail,
+  IsString,
+  IsEnum,
+  IsOptional,
+  Matches,
+} from 'class-validator';
+import { validationMsg } from '../constants/message.constants';
 
 export class CreateUserDto {
+  @IsString()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsEnum(UserRole)
   role: UserRole;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+    message: validationMsg.invalidPassword,
+  })
   password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const port = configService.get<string>('appConfig.port') ?? 3000;
   await app.listen(port);
